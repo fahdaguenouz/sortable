471edcd96447776f27927e4f951dbe6ed9829ccd
fahdaguenouz <faguenouz@gmail.com>
2025-03-03 14:51:50 +0000

    creating the pagination button

--- sortable.js
+++ sortable.js
@@ -1,36 +1,3 @@
-async function Sortable(){
-    let heros=[]
-
-    try{
-        const respo = await fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
-        //console.log(respo);
-        
-          if(!respo.ok){
-            throw new Error(`Response status: ${response.status}`);
-          }
-        heros= await respo.json()
-    }catch(e){
-        console.error(e.message);
-    }
-    
-    
-//console.log("kok",heros);
-const search=document.createElement("input")
-search.setAttribute("type","search")
-search.setAttribute("placeholder","search")
-
-search.className="search"
-document.body.appendChild(search)
-    const table =document.createElement("table")
-      table.appendChild(createHeader())
-      
-      table.appendChild(createBodytable(heros))
-      table.style.border="1px solid black"
-
-      document.body.appendChild(table)
-      
-
-}
 
 function createHeader() {
     const tr = document.createElement('tr');
@@ -148,6 +115,140 @@ function createHeader() {
     
     return tbody;
   }
+function createtable(heros){
+  console.log("create table");
+
+  const main =document.getElementById("main")
+  const table =document.createElement("table")
+  table.appendChild(createHeader())
+  
+  table.appendChild(createBodytable(heros))
+  table.style.border="1px solid black"
+main.appendChild(table)
+ 
+
+}
+function createSearch(){
+  const header=document.getElementById("header")
+ 
+  const search=document.createElement("input")
+      search.setAttribute("type","search")
+      search.setAttribute("placeholder","search")
+      search.className="search"
+      header.appendChild(search)
+  
+}
+
+function createPaggination(heros, perPage = 20) {
+  console.log("createpagination");
+  
+
+ const existingPagination = document.getElementById("pagination-container");
+ if (existingPagination) {
+   existingPagination.remove();
+ }
+  
+ 
+  const totalPages = Math.ceil(heros.length / perPage);
+console.log(totalPages);
+
+  
+  const paginationContainer = document.createElement("div");
+  const footer =document.getElementById("footer")
+console.log(footer);
+
+  paginationContainer.id = "pagination-container";
+ footer.append(paginationContainer)
+  // document.body.appendChild(footer);
+
+  // Create pagination buttons
+  for (let i = 1; i <= totalPages; i++) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    button.value = i;
+    button.className= "btn-pag";
+
+    // button.onclick = function() {
+      // const currentPage = parseInt(this.value);
+       // const start = (currentPage - 1) * perPage;
+       // const end = start + perPage;
+       // const paginatedHeros = heros.slice(start, end);
+       // const existingTable = document.querySelector("table");
+       // if (existingTable) {
+       //   existingTable.remove();
+     // }
+       // createtable(paginatedHeros);
+    // };
+    paginationContainer.appendChild(button);
+  }
+  return paginationContainer
+}
+function createSelect(heros){
+  let selectvalues=[10,20,50,100,"all results"] 
+const select=document.createElement("select")
+selectvalues.forEach((sel,i)=>{
+  const option=document.createElement("option")
+      option.textContent=sel
+      option.value = sel
+      option.id=sel
+      if(i==1){
+          option.setAttribute("selected",true)
+      }
+      select.appendChild(option)
+  })
+  const header=document.getElementById("header")
+  header.appendChild(select)
+  const initialFilteredHeros = SelectFilter(20, heros);
+  createtable(initialFilteredHeros); 
+
+
+  select.addEventListener("change", function () {
+    const filteredHeros = SelectFilter(this.value, heros);
+    const existingTable = document.querySelector("table"); 
+    if (existingTable) {
+      existingTable.remove();
+    }
+    createtable(filteredHeros); 
+  });
+
+return heros
+}
+
+function SelectFilter(e, heros) {
+  const filteredHeros = e === "all results" ? heros : heros.slice(0, parseInt(e));
+  createPaggination(heros, e === "all results" ? heros.length : parseInt(e));
+
+  return filteredHeros;
+}
+
+async function Sortable(){
+  let heros=[]
+
+  try{
+      const respo = await fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
+      //console.log(respo);
+      
+        if(!respo.ok){
+          throw new Error(`Response status: ${response.status}`);
+        }
+      heros= await respo.json()
+  }catch(e){
+      console.error(e.message);
+  }
+
+
+
+//console.log("kok",heros);
+  createSearch()
+    heros=createSelect(heros)
+
+}
+
+
+
+
+
+
 Sortable()
 
 
